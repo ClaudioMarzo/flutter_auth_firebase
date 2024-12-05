815c2dc56733d2e7a1f41075f6c3229344c5808d
Claudio Santarem <claudiosantarem@bemol.com.br>
2024-12-05 15:49:37 -0400

    style: renomeando cor

--- lib/core/ui/style/color_style.dart
+++ lib/core/ui/style/color_style.dart
@@ -16,9 +16,9 @@ class ColorsCustom {
   Color get darkblue => const Color.fromRGBO(0, 71, 165, 1);
   Color get lightblue => const Color.fromRGBO(206, 229, 255, 1);
   Color get red => const Color.fromRGBO(189, 0, 13, 1);
-  Color get gree => const Color.fromRGBO(35, 198, 83, 1);
-  Color get lightgree => const Color.fromRGBO(95, 217, 112, 1);
-  Color get darkgree => const Color.fromRGBO(0, 73, 26, 1);
+  Color get green => const Color.fromRGBO(35, 198, 83, 1);
+  Color get lightgreen => const Color.fromRGBO(95, 217, 112, 1);
+  Color get darkgreen => const Color.fromRGBO(0, 73, 26, 1);
   Color get grey => const Color.fromRGBO(221, 222, 223, 1);
   Color get darkyellow => const Color.fromARGB(255, 150, 100, 12);
   Color get yellow => const Color.fromRGBO(255, 186, 67, 1);
