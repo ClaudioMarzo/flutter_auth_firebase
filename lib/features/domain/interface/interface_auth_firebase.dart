e40f7d9f81a35da0f90d6a2079b17e81fb1e7f26
Claudio Santarem <claudiosantarem@bemol.com.br>
2024-12-06 14:19:40 -0400

    clenaup: reestruturando arquivos

--- lib/features/domain/interface/interface_auth_firebase.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-abstract class InterfaceAuthFireBase {
-  Future<User?> createUserWithEmailAndPassword(String email, String password);
-  Future<User?> signInWithEmailAndPassword(String email, String password);
-  Future<void> signOutFireBase();
-}
